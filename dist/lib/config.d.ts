b7d916db3c21725acebfaec486b06a85995e03da
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-12-23 21:19:45 +0700

    remove dist

--- dist/lib/config.d.ts
+++
@@ -1,8 +0,0 @@
-import { IConfig } from '../types/config.type';
-import { IFunction } from '../types/function.type';
-export declare const useConfig: () => {
-    setConfig: IFunction<Partial<IConfig>>;
-    config: {
-        components: Record<import('../types/config.type').ISchema["component"], import('vue').Component | null>;
-    };
-};
--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from 'vue';
-import { useConfig } from './lib/config';
-import type { IConfig, ISchema } from './types/config.type';
-import { useRender } from './lib/render';
-declare const VueFormSchema: Plugin;
-export default VueFormSchema;
-export { useConfig, IConfig, ISchema, useRender };
